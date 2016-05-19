b52de8f9cce5c0780a87689f319506e87cb2a1bb
Noah Kantrowitz <noah@coderanger.net>
2016-05-18 20:24:08 -0700

    Starting on the connection class and a simple implementation for the local plugin.

--- lib/airlift/connection/base.rb
+++ lib/airlift/connection/base.rb
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+require 'airlift/command'
+require 'airlift/file'
+
 
 module Airlift
   module Connection
@@ -21,6 +24,126 @@ module Airlift
     #
     # @since 1.0.0
     class Base
+      def initialize(**config)
+        @config = config
+      end
+
+      attr_reader :config
+
+      # @!group Primary API
+      # ===================
+
+      # Create a new command object.
+      #
+      # @see Airlift::Command#initialize
+      # @return [Airlift::Command]
+      def command(*cmd, **options)
+        Airlift::Command.new(self, *cmd, **options)
+      end
+
+      # Create a new file object.
+      #
+      # @see Airlift::File#initialize
+      # @return [Airlift::File]
+      def file(path, **options)
+        Airlift::File.new(self, path, **options)
+      end
+
+      # @!group Command Shortcuts
+      # =========================
+
+      # Create a command object with some defaults and run it. All arguments
+      # are the same as {#command}.
+      #
+      # @see #command
+      # @return [Airlift::Command]
+      def execute(*args)
+        command(*args).tap {|cmd| cmd.run_command }
+      end
+
+      # Create a command object with some defaults, run it, and raise an
+      # excption if it fails. All arguments are the same as {#command}.
+      #
+      # @see #command
+      # @return [Airlift::Command]
+      def execute!(*args)
+        execute(*args).tap {|cmd| cmd.error! }
+      end
+
+      # @!group Abstract API
+      # ====================
+
+      # Hook called during plugin loading the first time a plugin is used. Can
+      # be used to check installed utilities, config files, etc.
+      #
+      # @abstract
+      # @return [void]
+      def verify_dependencies
+        # Implemented by subclasses if needed.
+      end
+
+      # Download a given path. Yield data as it is available. Return true if the
+      # download succeeds and false if it fails.
+      #
+      # @abstract
+      # @param path [String] Path to download.
+      # @param block [Proc] Block to yield downloaded data to.
+      # @return [Boolean]
+      def download_file(path, &block)
+        raise NotImplementedError
+      end
+
+      # Upload a given path. Yields a callable which can be sent bytes to upload
+      # as they are ready. Return true if the upload succeeds and false if it
+      # fails.
+      #
+      # @abstract
+      # @param path [String] Path to download.
+      # @param block [Proc] Block to yield sender callable.
+      # @return [Boolean]
+      def upload_file(path, &block)
+        raise NotImplementedError
+      end
+
+      # Gather file stat data for a path. Return a File::Stat object or something
+      # compatible, or nil if the stat fails.
+      #
+      # @abstract
+      # @param path [String] Path to download.
+      # @param follow_symlink [Boolean] Follow symlinks or not.
+      # @return [Airlift::Stat, File::Stat, nil]
+      def stat(path, follow_symlink:)
+        raise NotImplementedError
+      end
+
+      # Synchronize a local directory to the remote side. This should behave
+      # like rsync with the remote side exactly matching the local side in the
+      # end. Return true if the sync succeeds and false if it fails.
+      #
+      # @abstract
+      # @param local_path [String] Local path to sync from.
+      # @param remote_path [String] Remote path to sync to.
+      # @return [Boolean]
+      def sync(local_path, remote_path)
+        raise NotImplementedError
+      end
+
+      # Delete a given path. Return true if the delete succeeds and false if it
+      # fails.
+      #
+      # @param path [String] Path to delete.
+      # @return [Boolean]
+      def delete(path)
+        raise NotImplementedError
+      end
+
+      # Tear down any resources that need it to clean up the connection.
+      #
+      # @abstract
+      # @return [void]
+      def close
+      end
+
     end
   end
 end
--- lib/airlift/connection/local.rb
+++ lib/airlift/connection/local.rb
@@ -23,6 +23,48 @@ module Airlift
     #
     # @since 1.0.0
     class Local < Base
+      # (see Base#download_file)
+      def download_file(path, &block)
+        return false unless ::File.readable?(path)
+        ::File.open(path, 'rb') do |fd|
+          while buf = fd.read(4096)
+            block.call(buf)
+          end
+        end
+        true
+      end
+
+      # (see Base#upload_file)
+      def upload_file(path, &block)
+        return false unless ::File.writable?(path)
+        ::File.open(path, 'wb') do |fd|
+          block.call(Proc.new {|data| fd.write(data) })
+        end
+        true
+      end
+
+      # (see Base#stat)
+      def stat(path, follow_symlink:)
+        if follow_symlink
+          ::File.stat(path)
+        else
+          ::File.lstat(path)
+        end
+      rescue SystemCallError
+        nil
+      end
+
+      def sync(local_path, remote_path)
+        raise 'boom'
+      end
+
+      def delete(path)
+        ::File.unlink(path)
+        true
+      rescue SystemCallError
+        false
+      end
+
     end
   end
 end
